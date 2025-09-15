8546550ffec05d735507c54e4662a17e4ba1863f
Hitarth <165199401+HitarthTrivedi@users.noreply.github.com>
2025-09-16 01:51:30 +0530

    Create trial.py

---
+++ trial.py
@@ -0,0 +1,12 @@
+import lazycook
+import asyncio
+# Create configured assistant
+config=lazycook.create_assistant(api_key:"Your_gemini_api_key",conversation_limit=70)
+
+
+# Run CLI
+asyncio.run(config.run_cli())
+
+
+# Or create assistant instance directly
+assistant=config.create_assistant()
